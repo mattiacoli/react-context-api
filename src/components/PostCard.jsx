8dceb130a8296bfcc189e8c8c86a0a5facfcf4ae
mattiacoli <mattia.coli@aol.com>
2025-04-03 15:31:24 +0200

    Refactor PostList component to use PostCard for rendering individual posts

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,16 @@
+export default function PostCard({ post }) {
+  return (
+    <>
+      <div key={post.id} className="col-md-4 mb-4">
+        <div className="card h-100 d-flex" style={{ minHeight: '350px' }}>
+          <img className="card-img-top" src={`http://localhost:3000/${post.image}`} alt={post.title} />
+          <div className="card-body d-flex flex-column justify-content-between">
+            <h4 className="card-title mb-4 ">{post.title}</h4>
+
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
+}
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,6 +1,7 @@
 
 import { useContext } from "react"
 import PostContext from "../contexts/PostContext"
+import PostCard from "./PostCard"
 
 export default function PostList() {
 
@@ -13,15 +14,7 @@ export default function PostList() {
 
         <div className="row">
           {posts.map(post => (
-            <div key={post.id} className="col-md-4 mb-4">
-              <div className="card h-100 d-flex" style={{ minHeight: '350px' }}>
-                <img className="card-img-top" src={`http://localhost:3000/${post.image}`} alt={post.title} />
-                <div className="card-body d-flex flex-column justify-content-between">
-                  <h4 className="card-title mb-4 ">{post.title}</h4>
-
-                </div>
-              </div>
-            </div>
+            <PostCard post={post} />
           ))}
 
         </div>
